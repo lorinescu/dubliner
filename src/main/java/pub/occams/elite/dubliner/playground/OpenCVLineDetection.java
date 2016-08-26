4a107723ebf5ebc668bb61d4dcf83eecee06354a
Lorin Scraba <lorin@occams.pub>
2016-08-26 15:19:40 +0200

    pipeline notes for image recognition based scraping

--- src/main/java/pub/occams/elite/dubliner/playground/OpenCVLineDetection.java
+++ src/main/java/pub/occams/elite/dubliner/playground/OpenCVLineDetection.java
@@ -16,9 +16,47 @@ public class OpenCVLineDetection {
 
     public static void main(String[] args) {
         testStandardHough();
+
+        /*
+
+        pipeline*:
+
+        1. pick an image from the image folder
+        2. if it is not a power play image goto 1.
+        3. find the coordinates** of the selected tab
+        5. crop the tab area, ocr the segment and apply corrections
+        6. if the selected tab is not Preparation | Control | Expansion goto 1.
+        7. find the coordinates of the power name
+        8. crop the power name, ocr the segment and apply corrections
+        9. if the power name is not Edmund | Winters | ..... goto 1.
+        10. find the coordinates for:
+            - system name
+            - preparation tab:
+            - control tab:
+                - costs area
+                - fortification area
+                - undermining area
+            - expansion tab:
+        11. ocr all segments and apply corrections to system names/numbers
+        12. produce a result object like:
+        {
+            maybe_debug: { misc data structures with detected coordinates, raw ocr results, refs to original file etc }
+            power: xyz
+            preparation: [..]
+            control: [..]
+            expansion: [..]
+        }
+        13. show the result
+        14. optionally clean-up the processed files from the image folder
+
+        ---
+        * pipeline - single thread if the tesseract instance is shared
+        ** find the coordinates - find a rectangle (x0,y0,x1,y1) that surrounds the area to be fed to ocr
+
+        */
     }
 
-    private static  void testStandardHough() {
+    private static void testStandardHough() {
         IplImage src = cvLoadImage(IMAGE_FILE, 0);
 
         if (src == null) {
@@ -34,10 +72,10 @@ public class OpenCVLineDetection {
 
         final CvMemStorage storage = cvCreateMemStorage(0);
 
-        final CvSeq lines = cvHoughLines2(dst, storage, CV_HOUGH_STANDARD, 1,RADIANS_IN_A_DEGREE, 300);
+        final CvSeq lines = cvHoughLines2(dst, storage, CV_HOUGH_STANDARD, 1, RADIANS_IN_A_DEGREE, 300);
 
         for (int i = 0; i < lines.total(); i++) {
-            final  CvPoint2D32f point = new CvPoint2D32f(cvGetSeqElem(lines, i));
+            final CvPoint2D32f point = new CvPoint2D32f(cvGetSeqElem(lines, i));
 
             float rho = point.x();
             float theta = point.y();
@@ -56,6 +94,7 @@ public class OpenCVLineDetection {
 
         showResultAndSrcImage(src, colorDst);
     }
+
     private static boolean isHorizontalOrVerticalLine(final float theta) {
         if (theta > 0.0001 && theta < Math.PI / 2 - 0.0001) {
             return false;
