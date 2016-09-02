fe8bbdd29d255f499aba6d126ef3d6bbbb2adbd3
Lorin Scraba <lorin@occams.pub>
2016-09-02 19:19:58 +0200

    code clean-up

--- src/main/java/pub/occams/elite/dubliner/application/ImageApiImpl.java
+++ src/main/java/pub/occams/elite/dubliner/application/ImageApiImpl.java
@@ -1,8 +1,12 @@
 package pub.occams.elite.dubliner.application;
 
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.apache.log4j.Logger;
 import org.bytedeco.javacpp.Pointer;
 import org.bytedeco.javacpp.opencv_core.*;
 import pub.occams.elite.dubliner.App;
+import pub.occams.elite.dubliner.correct.Corrector;
 import pub.occams.elite.dubliner.domain.ClassifiedImage;
 import pub.occams.elite.dubliner.domain.ImageType;
 import pub.occams.elite.dubliner.domain.InputImage;
@@ -15,6 +19,7 @@ import pub.occams.elite.dubliner.dto.ocr.*;
 import pub.occams.elite.dubliner.dto.settings.SettingsDto;
 import pub.occams.elite.dubliner.util.ImageUtil;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -30,10 +35,98 @@ import static pub.occams.elite.dubliner.domain.ImageType.*;
 import static pub.occams.elite.dubliner.util.ImageUtil.*;
 import static pub.occams.elite.dubliner.util.ImageUtil.invert;
 
-public class ImageApiImpl extends ImageApiBase {
+public class ImageApiImpl implements ImageApi {
+
+    private static final Logger LOGGER = Logger.getLogger(App.LOGGER_NAME);
+
+    private final boolean debug;
+    private int cnt = 0;
+
+    private final SettingsDto settings;
+    private final Corrector corrector;
+    private final Tesseract tessForNumbers = new Tesseract();
+    private final Tesseract tessForNames = new Tesseract();
 
     public ImageApiImpl(final SettingsDto settings, final boolean debug) {
-        super(settings, debug);
+        this.settings = settings;
+        this.corrector = new Corrector(settings.corrections);
+        this.debug = debug;
+
+        tessForNumbers.setDatapath(settings.ocr.tesseractForNumbers.dataPath);
+        tessForNumbers.setLanguage(settings.ocr.tesseractForNumbers.language);
+        settings.ocr.tesseractForNumbers.variables.forEach(tessForNumbers::setTessVariable);
+        settings.ocr.tesseractForNumbers.variables.forEach(tessForNumbers::setTessVariable);
+
+        tessForNames.setDatapath(settings.ocr.tesseractForSystemNames.dataPath);
+        tessForNames.setLanguage(settings.ocr.tesseractForSystemNames.language);
+        settings.ocr.tesseractForSystemNames.variables.forEach(tessForNames::setTessVariable);
+    }
+
+
+    @Override
+    public SettingsDto getSettings() {
+        return this.settings;
+    }
+
+    @Override
+    public Optional<InputImage> load(final File file) {
+        LOGGER.info("loading image:" + file.getAbsolutePath());
+
+        final Optional<BufferedImage> maybeImg = ImageUtil.readImageFromFile(file);
+        if (!maybeImg.isPresent()) {
+            return Optional.empty();
+        }
+
+        final BufferedImage img = maybeImg.get();
+
+        saveImageAtStage(img, file, "loading");
+
+        return Optional.of(new InputImage(file, maybeImg.get()));
+    }
+
+    protected BufferedImage saveImageAtStage(final BufferedImage image, final File imageFile, final String stage) {
+        if (!debug) {
+            return image;
+        }
+
+        try {
+            final String imageName = imageFile.getCanonicalPath().replace(File.separator, "-").replace(":", "-");
+            final String path = "out" + File.separator + imageName;
+            final File dir = new File(path);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            ImageIO.write(image, "png", new File(dir + File.separator + cnt + "-" + stage + ".png"));
+            cnt++;
+        } catch (IOException e) {
+            LOGGER.error("Failed to write debug image", e);
+        }
+        return image;
+    }
+
+
+    protected String ocrNumberRectangle(final BufferedImage image) {
+        LOGGER.info("starting number OCR");
+        try {
+            return tessForNumbers.doOCR(image);
+        } catch (TesseractException e) {
+            LOGGER.error("Error when ocr-ing image.", e);
+        }
+        return "";
+    }
+
+    protected String ocrRectangle(final BufferedImage image) {
+        LOGGER.info("starting OCR");
+        try {
+            final String str = tessForNames.doOCR(image);
+            if (null == str) {
+                return "";
+            }
+            return str;
+        } catch (Exception e) {
+            LOGGER.error("Error when ocr-ing image.", e);
+        }
+        return "";
     }
 
     private List<LineSegment> detectLines(final BufferedImage image, final File file, final int pixelResolution,
